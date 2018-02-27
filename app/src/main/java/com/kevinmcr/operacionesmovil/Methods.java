3c0c55c078f00a84bd206f02476794fe29f7d712
Kevin <castilloreyeskm@gmail.com>
2018-02-26 20:27:01 -0500

    Interface and add method

---
+++ app/src/main/java/com/kevinmcr/operacionesmovil/Methods.java
@@ -0,0 +1,11 @@
+package com.kevinmcr.operacionesmovil;
+
+/**
+ * Created by android on 26/02/2018.
+ */
+
+public class Methods {
+    public static double add(double n1, double n2){
+        return n1+n2;
+    }
+}
---
+++ app/src/main/java/com/kevinmcr/operacionesmovil/Principal.java
@@ -0,0 +1,33 @@
+package com.kevinmcr.operacionesmovil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Principal extends AppCompatActivity {
+    private EditText n1,n2;
+    private TextView res;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        n1 = findViewById(R.id.txtNumeroUno);
+        n2 = findViewById(R.id.txtNumeroDos);
+
+        res = findViewById(R.id.lblResultado);
+    }
+
+
+
+    public void calcular(View v){
+        double num1, num2, resultado;
+        num1 =  Double.parseDouble(n1.getText().toString());
+        num2 =  Double.parseDouble(n2.getText().toString());
+
+        resultado = Methods.add(num1,num2);
+        res.setText("" + resultado);
+    }
+}
