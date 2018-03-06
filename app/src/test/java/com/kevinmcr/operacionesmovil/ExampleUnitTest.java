288f23c6b24ef7f462260a86568fc53e7fa452ca
Kevin <castilloreyeskm@gmail.com>
2018-03-05 21:26:39 -0500

    Testing

--- app/src/test/java/com/kevinmcr/operacionesmovil/ExampleUnitTest.java
+++ app/src/test/java/com/kevinmcr/operacionesmovil/ExampleUnitTest.java
@@ -10,8 +10,35 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+//    public void addition_isCorrect() throws Exception {
+//        assertEquals(4, 2 + 2);
+//    }
+    @Test // Siempre debe preceder al metodo tester
+    public void sumar_dos_numeros_correctamente(){
+        double a = 4, b = 5;
+        double res = Methods.sumar(a,b);
+        assertEquals(9,res,0);
+    }
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void restar_dos_numeros_correctamente(){
+        double a = 4, b = 5;
+        double res = Methods.restar(a,b);
+        assertEquals(-1,res,0);
+    }
+
+    @Test
+    public void multiplicar_dos_numeros_correctamente(){
+        double a = 4, b = 5;
+        double res = Methods.multiplicar(a,b);
+        assertEquals(20,res,0);
+    }
+
+    @Test
+    public void dividir_dos_numeros_correctamente(){
+        double a = 4, b = 2;
+        double res = Methods.dividir(a,b);
+        assertEquals(2,res,0);
     }
 }
